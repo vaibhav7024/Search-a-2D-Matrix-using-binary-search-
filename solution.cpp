936493a927bfdde43947feb8f679bc0544acd4ab
vaibhav7024 <vjaiswal7024@gmail.com>
2026-05-15 12:09:35 +0530

    Create solution.cpp

---
+++ solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int row =0, col= cols-1;
+        while(row<rows && col>-1){
+            int curr = matrix[row][col];
+            if(curr== target) return true;
+            if(curr>target) col--;
+            else row++;
+        }
+        return false;
+    }
+};
